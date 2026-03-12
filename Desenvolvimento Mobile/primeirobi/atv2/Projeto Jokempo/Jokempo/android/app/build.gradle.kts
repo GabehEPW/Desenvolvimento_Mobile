26a026206d563846d885d73a68ed695ee4efb928
GabehEPW <gabriel.epw@outlook.com>
2026-03-12 13:32:24 -0300

    Remove pasta .idea do versionamento

--- Desenvolvimento Mobile/primeirobi/atv2/Projeto Jokempo/Jokempo/android/app/build.gradle.kts
+++ Desenvolvimento Mobile/primeirobi/atv2/Projeto Jokempo/Jokempo/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.desenvolvimentomobile"
+    namespace = "com.example.Jokempo"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.desenvolvimentomobile"
+        applicationId = "com.example.Jokempo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- Desenvolvimento Mobile/primeirobi/atv2/Projeto Jokempo/Jokempo/test/widget_test.dart
+++ Desenvolvimento Mobile/primeirobi/atv2/Projeto Jokempo/Jokempo/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:desenvolvimentomobile/main.dart';
+import 'package:Jokempo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
